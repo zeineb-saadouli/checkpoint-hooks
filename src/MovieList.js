c27f7c116f78abab09b0b2a1b8232e66e1bf42f2
zeineb-saadouli <saadoulizeineb1@gmail.com>
2024-12-20 16:45:16 +0100

    first commit

--- src/MovieList.js
+++ src/MovieList.js
@@ -19,7 +19,7 @@ import MovieCard from './MovieCard'
 function MovieList(props) {
     const{movies}=props;
   return (
-    <div>
+    <div className='movie-list'>
         {
             movies.map((movie)=>(
                 <MovieCard key={movie.id} movie={movie}/>
